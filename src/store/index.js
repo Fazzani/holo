558e79ccb18d7a9d318c16a215f09803af74a1c0
Fazzani <tunisienheni@outlook.com>
2018-09-08 13:32:06 +0200

    add examples

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,15 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers/index";
 import playlistReducer from "./reducers/playlistsReducer";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const allReducers = combineReducers({
     root: rootReducer,
     playlist: playlistReducer
 });
 // const store = createStore(allReducers, window.devToolsExtension && window.devToolsExtension(), applyMiddleware(logger));
-const store = compose(
-    applyMiddleware(thunk, logger)
-    //   window.devToolsExtension && window.devToolsExtension(),
-)(createStore)(allReducers);
+const store = composeWithDevTools(applyMiddleware(thunk, logger))(createStore)(
+    allReducers
+);
 
 export default store;
